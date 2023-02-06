55e56e3e1ae2270900bba48bf1521da0558d819e
Abla <la_rabia@esi.dz>
2023-02-07 00:14:56 +0100

    adding images

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import kotlin.random.Random
@@ -25,7 +26,15 @@ class MainActivity : AppCompatActivity() {
     private fun diceRoll() {
 
         var randomInt=java.util.Random().nextInt(6)+1
-        val text:TextView=findViewById(R.id.text_result)
-        text.text=randomInt.toString()
+        val imageres=when (randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        val dice_im:ImageView=findViewById(R.id.dice_image)
+        dice_im.setImageResource(imageres)
     }
 }
